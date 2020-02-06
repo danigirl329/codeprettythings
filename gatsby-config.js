569212b6edcfd94a8797b692bcdb950fead4b2db
danigirl329 <greaves@trustarts.orgl>
2020-02-06 15:32:31 -0500

    Add gh-pages Add favicon Add start url

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `A sassy frontend developer who loves code, coffee, and puggles.`,
     author: `@danigirl329`,
     pathPrefix: "/codeprettythings",
+    assetPrefix: `https://danigirl329.github.io/codeprettythings`
   },
   plugins: [
     `gatsby-plugin-postcss`,
@@ -32,11 +33,11 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
+        start_url: `https://danigirl329.github.io/codeprettythings/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
