9a2a91e2c801a51ca077a74c308a1f282cd6cea6
danigirl329 <greaves@trustarts.orgl>
2020-02-06 13:30:59 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans'],
+        cursive: ['Bubblegum Sans', 'cursive'],
+      },
+      colors: {
+        pink: '#FFECEB',
+        green: '#44D7B5',
+      },
+    }
+  },
+  variants: {},
+  plugins: []
+}
