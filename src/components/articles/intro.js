e6b5de6dafe730aba9fc1133bcaffe7b4cda2e5c
danigirl329 <danigreaves@gmail.com>
2021-03-12 22:27:56 -0500

    Add array of hobbies and loop them in the intro

--- src/components/articles/intro.js
+++ src/components/articles/intro.js
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types"
 import me from "../../images/me.png"
 
 const Intro = ({ children }) => {
+  var hobbies = ["coffee enthusiast", "dog cuddler", "gravel biker"]
+  const [seconds, setSeconds] = useState(0)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(second => (second === 2 ? 0 : second + 1));
+    }, 2000);
+    return () => clearInterval(interval);
+    }, []);
+    useEffect(() => {
+      console.log("seconds", seconds);
+    }, [seconds]);  
     return (
       <>
         <article class="container text-center m-auto max-w-md min-h-screen" id="intro">
@@ -10,7 +21,8 @@ const Intro = ({ children }) => {
             <img class="rounded-full h-32 w-32 mx-auto" src={me} alt="My face on a lighted background" />
             <h2>I'm Danielle</h2>
             <h3>I'm a frontend <span class="text-green">dev</span>eloper</h3>
-            <h4>and a <span class="text-green" id="roulette">coffee enthusiast</span></h4>
+            <h4>and a </h4>
+            <h4><span class="text-green" id="roulette">{hobbies[seconds]}</span></h4>
         </article>
       </>
     )
